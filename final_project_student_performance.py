88479967cad4cbe4d2780dd19b8a7a999d606558
Xiang Han <xh336@cornell.edu>
2024-05-02 21:04:53 -0400

    Make Streamlit Run

---
+++ final_project_student_performance.py
@@ -0,0 +1,43 @@
+import streamlit as st                  # pip install streamlit
+
+st.markdown("# Practical Applications of Machine Learning (PAML)")
+
+#############################################
+
+st.markdown("### Final Project")
+
+#############################################
+
+st.markdown("Welcome to the Practical Applications in Machine Learning (PAML) Course! You will build a series of end-to-end ML pipelines, working with various data types and formats, and will need to engineer your system to support training, testing, and deploying ML models.")
+
+
+st.markdown("""The goal of this assignment is to demonstrate your programming skills and ability to build a simple application using the Streamlit library. As a machine learning (ML) practitioner, you are expected to have basic knowledge of programming, learning new programming languages, and making things work off-the-fly. You will build a basic user interface that walks users through an end-to-end ML pipeline for a case studies involving fundamental ML algorithms including regression, classification, clustering, and deep learning.
+
+The learning outcomes for this assignment are to end an app using: 
+- Text Elements
+- Data elements
+- Chart Elements
+- Input Widgets
+- Media Elements
+- Layouts and Containers
+- State Management
+""")
+
+st.markdown("""You will build a web application that helps Machine Learning Practitioners through an end-to-end ML pipeline.
+
+    1. Explore and visualize the data to gain insights.
+    2. Preprocess and prepare the data for machine learning algorithms.
+    3. Select a model and train it. Fine-tune your model.
+    4. Test Model.  Evaluate models using standard metrics.
+    5. Deploy your application (present your solution). Launch, monitor, and maintain your system.
+""")
+
+st.markdown(""" Future homework assignments will engage students in more an in depth implementation and discussion of ethical implications of four applications.
+    
+    1. Regression for Predicting Housing Prices
+    2. Clustering for Document Retrieval
+    3. Classification for Product Recommendation
+    4. Deep Learning for Image Search
+""")
+
+st.markdown("Click **Explore Dataset** to get started.")
